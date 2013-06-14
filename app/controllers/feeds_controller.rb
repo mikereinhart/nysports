39a3285a8cd69324ac0f97c9c80798664f5f2981
Michael Reinhart <mreinhart88@gmail.com>
2013-06-13 23:18:13 -0400

    got most all views working. still left is team_show and the feed ones besides index. Do we even need an edit and new page for feeds? they are auto-populated by sites so we dont manually enter them

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,20 +5,37 @@ class FeedsController < ApplicationController
 	end
 
 	def new
+		@feed = Feed.new
 	end
 
 	def create
+		@feed = Feed.new(params['feed'])
+		if @feed.save
+			redirect_to feeds_path
+		else
+			render :new
+		end
 	end
 
 	def show
+		@feed = Feed.find(params[:id])
 	end
 
 	def edit
+		@feed = Feed.find(params[:if])
 	end
 
 	def update
+		@feed = Feed.find(params[:id])
+		if @feed.update_attributes(params['feed'])
+			redirect_to feeds_path
+		else
+			render :new
+		end
 	end
 
 	def destroy
+		Feed.find(params[:id]).delete
+		redirect_to feeds_path
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,33 @@ s7 = Site.create(name: 'Knicks ESPN', site_url: 'http://espn.go.com/nba/team/_/n
 s8 = Site.create(name: 'Nets ESPN', site_url: 'http://espn.go.com/nba/team/_/name/bkn/brooklyn-nets')
 s9 = Site.create(name: 'Jets Bleacher Report', site_url: 'http://bleacherreport.com/new-york-jets')
 
+
+#Jets
+j1 = Feed.create(name: 'NYJ Official', feed_url: 'http://www.newyorkjets.com/cda-web/rss-module.htm?tagName=News')
+j2 = Feed.create(name: 'NJ.com Jets', feed_url: 'http://blog.nj.com/jets_impact/atom.xml') 
+
+#Giants
+g1 = Feed.create(name: 'NJ.com Giants', feed_url: 'http://blog.nj.com/giants_impact/atom.xml')
+g2 = Feed.create(name: 'NYPost Giants', feed_url: 'http://www.nypost.com/rss/giants.xml') 
+
+#nets
+n1 = Feed.create(name: 'NJ.com Nets', feed_url: 'http://blog.nj.com/nets_impact/atom.xml') 
+n2 = Feed.create(name: 'NJ Daily News Nets', feed_url: 'http://www.nydailynews.com/sports/basketball/nets/index_rss.xml') 
+
+#knicks
+k1 = Feed.create(name: 'NYPost Knicks', feed_url: 'http://www.nypost.com/rss/knicks.xml')
+k2 = Feed.create(name: 'Yahoo Knicks', feed_url: 'http://sports.yahoo.com/nba/teams/nyk/rss.xml') 
+
+#yankees
+y1 = Feed.create(name: 'NJ.com Yankees', feed_url: 'http://blog.nj.com/yankees_main/atom.xml')
+y2 = Feed.create(name: 'Yahoo Yankees', feed_url: 'http://sports.yahoo.com/mlb/teams/nyy/rss.xml') 
+
+#mets
+m1 = Feed.create(name: 'NYPost Mets', feed_url: 'http://www.nypost.com/rss/mets.xml')
+m2 = Feed.create(name: 'NY Daily News Mets', feed_url: 'http://www.nydailynews.com/sports/baseball/mets/index_rss.xml') 
+
+
+
 =begin
 u1.teams << t1 << t3 << t6
 u2.teams << t2 << t4 << t5
