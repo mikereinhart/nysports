287975c7f4f921cf575d20218789ceb50410130a
Michael Reinhart <mreinhart88@gmail.com>
2013-06-13 23:32:35 -0400

    did stuff. added nav bar. added sessions stuff. going to add styling then probably be done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+def create
+  user = User.find_by_email(params[:email])
+  #puts 'located user: ' + user.name
+  authenticated_user = user.authenticate(params[:password])
+  if authenticated_user
+    session[:user_id] = authenticated_user.id
+    message = 'You are authenticated!'
+    redirect_to root_path
+  else
+    redirect_to sessions_new_path
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  current_user_id = nil
+  redirect_to sessions_new_path
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Nysports::Application.routes.draw do
   
- resources :users, :teams, :sites, :feeds
- root :to => 'home#index'
-
+	resources :users, :teams, :sites, :feeds
+	root :to => 'home#index'
+	get 'sessions/new' => 'sessions#new'
+	post 'sessions' => 'sessions#create'
+	delete 'logout' => 'sessions#destroy'
 end
