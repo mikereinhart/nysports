ac5dd96b176f20d70d3e74b714ceed36ce9ac81c
Bernard Doherty <bernard.doherty@icloud.com>
2013-06-15 16:19:41 -0400

    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-# User.delete_all
-# Team.delete_all
- Site.delete_all
- Feed.delete_all
+User.delete_all
+Team.delete_all
+Site.delete_all
+Feed.delete_all
 
 
-# u1 = User.create(name: 'Mike', password: 'reinhart', email: 'mike@sample.com')
-# u2 = User.create(name: 'Bernard', password: 'doherty', email: 'bernard@sample.com')
-# #u3 = User.create(name: '', password: '', email: '')
+u1 = User.create(name: 'Mike', password: 'reinhart', email: 'mike@sample.com')
+u2 = User.create(name: 'Bernard', password: 'doherty', email: 'bernard@sample.com')
+#u3 = User.create(name: '', password: '', email: '')
 
 
-# t1 = Team.create(name: 'Giants', logo_url: '', official_url: 'http://www.giants.com/', twitter_url: 'https://twitter.com/Giants', facebook_url: 'https://www.facebook.com/newyorkgiants')
-# t2 = Team.create(name: 'Jets', logo_url: '', official_url: 'http://www.newyorkjets.com/', twitter_url: 'https://twitter.com/nyjets', facebook_url: 'https://www.facebook.com/Jets')
-# t3 = Team.create(name: 'Yankees', logo_url: '', official_url: 'http://newyork.yankees.mlb.com/index.jsp?c_id=nyy', twitter_url: 'https://twitter.com/Yankees', facebook_url: 'https://www.facebook.com/Yankees')
-# t4 = Team.create(name: 'Mets', logo_url: '', official_url: 'http://newyork.mets.mlb.com/index.jsp?c_id=nym', twitter_url: 'https://twitter.com/Mets', facebook_url: 'https://www.facebook.com/Mets')
-# t5 = Team.create(name: 'Knicks', logo_url: '', official_url: 'http://www.nba.com/knicks/', twitter_url: 'https://twitter.com/nyknicks', facebook_url: 'https://www.facebook.com/NYKnicks')
-# t6 = Team.create(name: 'Nets', logo_url: '', official_url: 'http://www.nba.com/nets/', twitter_url: 'https://twitter.com/BrooklynNets', facebook_url: 'https://www.facebook.com/BrooklynNets')
+t1 = Team.create(name: 'Giants', logo_url: '', official_url: 'http://www.giants.com/', twitter_url: 'https://twitter.com/Giants', facebook_url: 'https://www.facebook.com/newyorkgiants')
+t2 = Team.create(name: 'Jets', logo_url: '', official_url: 'http://www.newyorkjets.com/', twitter_url: 'https://twitter.com/nyjets', facebook_url: 'https://www.facebook.com/Jets')
+t3 = Team.create(name: 'Yankees', logo_url: '', official_url: 'http://newyork.yankees.mlb.com/index.jsp?c_id=nyy', twitter_url: 'https://twitter.com/Yankees', facebook_url: 'https://www.facebook.com/Yankees')
+t4 = Team.create(name: 'Mets', logo_url: '', official_url: 'http://newyork.mets.mlb.com/index.jsp?c_id=nym', twitter_url: 'https://twitter.com/Mets', facebook_url: 'https://www.facebook.com/Mets')
+t5 = Team.create(name: 'Knicks', logo_url: '', official_url: 'http://www.nba.com/knicks/', twitter_url: 'https://twitter.com/nyknicks', facebook_url: 'https://www.facebook.com/NYKnicks')
+t6 = Team.create(name: 'Nets', logo_url: '', official_url: 'http://www.nba.com/nets/', twitter_url: 'https://twitter.com/BrooklynNets', facebook_url: 'https://www.facebook.com/BrooklynNets')
 
 
 # s1 = Site.create(name: 'Pro Football Talk', site_url: "http://profootballtalk.nbcsports.com/")
