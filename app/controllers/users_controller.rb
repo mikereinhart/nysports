6cd10dfe03e95a7319c55d48b029702b71c5cf69
Michael Reinhart <mreinhart88@gmail.com>
2013-06-16 22:11:54 -0400

    most of the sites styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController <ApplicationController
 	def update
 		@user = User.find(params[:id])
 		if @user.update_attributes(params['user'])
-			redirect_to users_path
+			redirect_to user_path(current_user)
 		else
 			render :new
 		end
